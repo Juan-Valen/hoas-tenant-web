12e54d5e3b2ae53f15cfb764d8aa1ffa9c65225f
john <juan.valenz.101@hotmail.com>
2025-09-16 15:11:40 +0300

    Server setup

---
+++ server/models/reservationModel.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const reservationSchema = new Schema(
+    {
+        reserved_id: {
+            type: String,
+            required: true,
+        },
+        start: {
+            type: String,
+            required: true
+        },
+        end: {
+            type: String,
+            required: true,
+        },
+        reserved_by: {
+            type: String,
+            required: true,
+        },
+    },
+    { timestamps: true }
+);
+
+module.exports = mongoose.model("Reservation", reservationSchema);
